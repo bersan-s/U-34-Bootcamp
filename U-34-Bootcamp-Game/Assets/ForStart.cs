be9a6f04383313f28bd61be91b3ac9eaa0ec016b
bcgkky <bcgkky1@gmail.com>
2023-07-18 00:45:24 +0300

    lastUpdate_v3

---
+++ U-34-Bootcamp-Game/Assets/ForStart.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ForStart : MonoBehaviour
+{
+    
+    
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+   
+}
